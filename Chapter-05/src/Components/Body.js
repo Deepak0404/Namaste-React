fffe5246d2a117dd36d1159ee70aadc34dc283f9
Deepak Saini <deepaksaini.net@gmail.com>
2023-07-08 12:32:48 +0530

    Chapter 05 files

--- Chapter-05/src/Components/Body.js
+++ Chapter-05/src/Components/Body.js
@@ -1,51 +1,56 @@
-import { restaurantList } from "../contents";
-import RestaurantsCard from "./RestaurantsCard";
-import { useState } from "react";
+import { restaurantList } from '../contents'
+import RestaurantsCard from './RestaurantsCard'
+import { useState } from 'react'
 
 // What is state?
 // What is React Hook? - Are normal JS functions
-// What is useState? - Used for two way data binding
+// What is useState?
+//  - Used to create local varibles in your application
+//  - Used for two way data binding
 
 const Body = () => {
+    // normal JS variables
+    // const searchText = "KFC";
 
-// normal JS variables
-// const searchText = "KFC";
+    // React state variables
+    // This function returns an array and the first element of the array is variable name
+    // searchText is local state variable
 
-// React state variables
-// This function returns an array and the first element of the array is variable name
-// searchText is local state variable
+    const [searchText, setSearchText] = useState('KFC') // return = [variable name, function to update the variable]
+    const [searchClicked, setSearchClicked] = useState('false')
 
-const [searchText, setSearchText] = useState("KFC"); // return = [variable name, function to update the variable]
-const [searchClicked, setSearchClicked] = useState("false");
-
-    return(
+    console.log('Rerender')
+    return (
         <>
             <div className="search-container">
-                <input 
+                <input
                     type="text"
-                    className="search-input" 
-                    value={searchText} 
+                    className="search-input"
+                    value={searchText}
                     placeholder=""
                     onChange={(e) => {
-                        setSearchText(e.target.value);
+                        setSearchText(e.target.value)
                     }}
                 />
-                <button 
-                    className="search-btn" 
-                    onClick={()=>{searchClicked === "true" ? setSearchClicked("false") : setSearchClicked("true")}}
-                    >
-                Search</button>
+                <button
+                    className="search-btn"
+                    onClick={() => {
+                        searchClicked === 'true'
+                            ? setSearchClicked('false')
+                            : setSearchClicked('true')
+                    }}
+                >
+                    Search
+                </button>
             </div>
             <h2>{searchClicked}</h2>
             <div className="restaurant-list">
-                {
-                    restaurantList.map(restaurant => {
-                        return <RestaurantsCard {...restaurant.data} key={restaurant.data.id}/>
-                    })
-                }
+                {restaurantList.map((restaurant) => {
+                    return <RestaurantsCard {...restaurant.data} key={restaurant.data.id} />
+                })}
             </div>
         </>
     )
 }
 
-export default Body;
+export default Body
